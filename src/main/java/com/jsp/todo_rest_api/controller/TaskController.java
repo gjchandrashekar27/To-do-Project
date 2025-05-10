1aae184d2cefa72d619d43e8891a54302f234eb5
chandrashekar Gj <chandrashekargj27@gmail.com>
2025-05-10 23:30:33 +0530

    Deleted Task Completed

--- src/main/java/com/jsp/todo_rest_api/controller/TaskController.java
+++ src/main/java/com/jsp/todo_rest_api/controller/TaskController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,6 +41,12 @@ public class TaskController {
 	@GetMapping("/{id}")
 	public ResponseEntity<Map<String, Object>>  fetchTaskById(@RequestHeader(required = false) String sessionId, @PathVariable Long id){
 		return ResponseEntity.status(HttpStatus.OK).body(taskService.fetchTaskById(sessionId,id));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Map<String, Object>> deleteTaskById(@RequestHeader(required = false) String sessionId,
+			@PathVariable Long id) {
+		return ResponseEntity.status(HttpStatus.OK).body(taskService.deleteTaskById(sessionId, id));
 	}
 
 }
